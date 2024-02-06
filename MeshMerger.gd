ff6f0375eaf35c1a47ff1b9ede36534b244925cf
AdilDevStuff <94475453+AdilDevStuff@users.noreply.github.com>
2024-02-06 17:58:20 +0500

    Removed toggle_children_visibility bool

--- MeshMerger.gd
+++ MeshMerger.gd
@@ -1,97 +1,86 @@
-extends MeshInstance
+@tool
 class_name MeshMerger
-tool
+extends MeshInstance3D
 
 var result_mesh:ArrayMesh
 var result_mesh_material:Material
 
+@export var collision_parent: Node
 
-export (NodePath) var collision_parent_path
-onready var collision_parent:Node
+@export var btn_merge_meshes: bool: 
+	set(value): merge_meshes(value)
+	get: return btn_merge_meshes
 
-export (bool) var btn_merge_meshes setget merge_meshes
-export (bool) var btn_clean_meshes setget clean_meshes
+@export var btn_clean_meshes: bool:
+	set(value): clean_meshes(value)
+	get: return btn_clean_meshes
 
-export (bool) var toggle_children_visibility setget set_toggle_children_visibility
-export (bool) var delete_child_meshes_on_play setget set_delete_child_meshes_on_play
+@export var delete_child_meshes_on_play: bool:
+	set(value):
+		set_delete_child_meshes_on_play(value)
+	get: return delete_child_meshes_on_play
 
-# warning-ignore:unused_argument
-func merge_meshes(value):
-	if !Engine.editor_hint:
+func merge_meshes(_value):
+	if !Engine.is_editor_hint():
 		return
 
 	if result_mesh:
 		result_mesh.clear_surfaces()
 	
-	if collision_parent_path:
-		collision_parent = get_node(collision_parent_path)
+	if collision_parent:
 		clean_collisions()
 
 	var _surface_tool := SurfaceTool.new()
 
 	for node in get_children():
-		if node is MeshInstance:
+		if node is MeshInstance3D:
 			_surface_tool.append_from(node.mesh, 0, node.transform)
 			grab_material(node)
 			generate_collisions(node)
 
 		result_mesh = _surface_tool.commit()
 	
-	set_toggle_children_visibility(false)
 	self.mesh = result_mesh
 	self.material_override = result_mesh_material
 
-# warning-ignore:unused_argument
-func clean_meshes(value):
-	if !Engine.editor_hint:
+func clean_meshes(_value):
+	if !Engine.is_editor_hint():
 		return
-
-	if collision_parent_path:
-		collision_parent = get_node(collision_parent_path)
 
 	mesh = ArrayMesh.new()
 	clean_collisions()
-	set_toggle_children_visibility(true)
-
 
 func grab_material(node):
-	if node.get_surface_material(0):
-		result_mesh_material = node.get_surface_material(0)
+	if node.get_active_material(0):
+		result_mesh_material = node.get_active_material(0)
 	elif node.mesh.surface_get_material(0):
-		result_mesh_material = node.mesh.surface_get_material(0)
+		result_mesh_material = node.mesh.get_active_material(0)
 
 func generate_collisions(node):
-	if !collision_parent or !collision_parent_path:
+	if !collision_parent:
 		return
 	for child in node.get_children():
-		if child is StaticBody and child.get_child_count() > 0:
+		if child is StaticBody3D and child.get_child_count() > 0:
 			for grandchild in child.get_children():
-				if grandchild is CollisionShape:
-					var new_col := CollisionShape.new()
+				if grandchild is CollisionShape3D:
+					var new_col := CollisionShape3D.new()
 					new_col.global_transform = child.global_transform
 					collision_parent.add_child(new_col)
 					new_col.shape = grandchild.shape
 					new_col.set_owner(get_tree().get_edited_scene_root())
 
 func clean_collisions():
-	if !collision_parent or !collision_parent_path:
+	if !collision_parent:
 		return
 	if collision_parent.get_child_count() > 0:
 		for child in collision_parent.get_children():
 			child.queue_free()
 
-func set_toggle_children_visibility(value):
-	toggle_children_visibility = value
-	for node in get_children():
-		if node is MeshInstance:
-			node.visible = value
-
 func set_delete_child_meshes_on_play(value):
 	delete_child_meshes_on_play = value
-
 
 func _ready():
 	if delete_child_meshes_on_play:
 		for node in get_children():
-			if node is MeshInstance:
+			if node is MeshInstance3D:
 				node.queue_free()
